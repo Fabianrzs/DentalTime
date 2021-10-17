60d54a38fa9fc0288e91cf46212c9a8edfdf14ec
Fdamirez <fdramirez@unicesar.edu.co>
2021-10-16 21:19:42 -0500

    Modelo Del contexto Creado

---
+++ DAL/DentalTimeContext.cs
@@ -0,0 +1,31 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DentalTimeContext:DbContext
+    {
+        public DentalTimeContext(DbContextOptions contextOptions): base(contextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Enlace de la Database
+        }
+
+        public DbSet<Agenda> Agendas { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<ConsultaClinica> Consultas { get; set; }
+        public DbSet<HistoriaClinica> HistoriasClinicas { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Servicio> Servicios { get; set; }
+    }
+}
