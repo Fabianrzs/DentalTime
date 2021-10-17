34a381beb1cd4881597759e1310620200b8d5d35
andres pertuz <andrespertuz16@hotmail.com>
2021-10-17 16:35:41 -0500

    Cambios

--- DentalTime/Startup.cs
+++ DentalTime/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace DentalTime
 {
@@ -20,6 +22,8 @@ namespace DentalTime
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DentalTimeContext>(p => p.UseSqlServer(connectionString));
             services.AddSwaggerGen();
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
