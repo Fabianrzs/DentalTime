672a8addce02785b1a75588744d5a5a3cc636214
andres pertuz <andrespertuz16@hotmail.com>
2021-10-18 09:25:07 -0500

    Cambios

---
+++ BLL/PacienteService.cs
@@ -0,0 +1,61 @@
+﻿using DAL;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PacienteService
+    {
+        private readonly DentalTimeContext _context;
+
+        public PacienteService(DentalTimeContext context)
+        {
+            _context = context;
+        }
+
+        public PacienteResponse Guardar(Paciente paciente)
+        {
+            try
+            {
+                var buscarPaciente = _context.Pacientes.Find(paciente.NoDocumeto);
+                if (buscarPaciente != null)
+                {
+                    return new PacienteResponse("Error el paciente ya se encuentra registrada");
+                }
+                _context.Pacientes.Add(paciente);
+                _context.SaveChanges();
+                return new PacienteResponse(paciente);
+            }
+            catch (Exception e)
+            {
+                return new PacienteResponse("Error en la aplicacion"+e.Message);
+            }
+        }
+
+    }
+
+    public class PacienteResponse
+    {
+        public Paciente Paciente { get; set; }
+        public string Mensaje { get; set; }
+        public bool Error { get; set; }
+
+
+        public PacienteResponse(string mensaje)
+        {
+            Mensaje = mensaje;
+            Error = true;
+        }
+
+        public PacienteResponse(Paciente paciente)
+        {
+            Paciente = paciente;
+            Error = false;
+        }
+
+    }
+}
---
+++ DentalTime/Controllers/PacienteController.cs
@@ -0,0 +1,53 @@
+﻿using BLL;
+using DAL;
+using DentalTime.Models;
+using Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentalTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        private PacienteService _pacienteService;
+
+        public PacienteController(DentalTimeContext context)
+        {
+            _pacienteService = new PacienteService (context);
+        }
+
+        public ActionResult<Paciente> Guardar(PacienteImputModel pacienteImput)
+        {
+            Paciente paciente = MapearPaciente(pacienteImput);
+            PacienteResponse pacienteResponse = _pacienteService.Guardar(paciente);
+            if (pacienteResponse.Error)
+            {
+                return BadRequest(pacienteResponse.Mensaje);
+            }
+            return Ok(pacienteResponse.Paciente);
+        }
+
+        private Paciente MapearPaciente(PacienteImputModel pacienteImput)
+        {
+            var paciente = new Paciente();
+            paciente.TipoDocumento = pacienteImput.TipoDocumento;
+            paciente.NoDocumeto = pacienteImput.NoDocumeto;
+            paciente.Nombres = pacienteImput.Nombres;
+            paciente.Apellidos = pacienteImput.Apellidos;
+            paciente.Sexo = pacienteImput.Sexo;
+            paciente.FechaNacimiento = pacienteImput.FechaNacimiento;
+            paciente.PaisNacimiento = pacienteImput.PaisNacimiento;
+            paciente.CiudadNacimiento = pacienteImput.CiudadNacimiento;
+            paciente.TipoSaguineo = pacienteImput.TipoSaguineo;
+            paciente.NumeroTelefonico = pacienteImput.NumeroTelefonico;
+            paciente.CorreoElectronico = pacienteImput.CorreoElectronico;
+            return paciente;
+        }
+    }
+}
