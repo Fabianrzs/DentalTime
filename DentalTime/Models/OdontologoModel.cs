f1fd6de409a50e059d4940237d9a033864f2497f
Fdamirez <fabianramirez0072@gmail.com>
2021-12-02 16:36:20 -0500

    Apis Odontologo +Servicio

---
+++ DentalTime/Models/OdontologoModel.cs
@@ -0,0 +1,28 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentalTime.Models
+{
+    public class OdontologoInputModel
+    {
+        public string TipoDocumento { get; set; }
+        public string NoDocumento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+    }
+    public class OdontologoViewModel: OdontologoInputModel
+    {
+        public OdontologoViewModel(Odontologo odontologo)
+        {
+            TipoDocumento = odontologo.TipoDocumento;
+            NoDocumento = odontologo.NoDocumento;
+            Nombres = odontologo.Nombres;
+            Apellidos = odontologo.Apellidos;
+        }
+
+    }
+}
